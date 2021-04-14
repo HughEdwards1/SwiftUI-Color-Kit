4c825694a34f9db5456be9479b7e83df3577f3ea
Hugh Edwards <hugh@hughedwards.dev>
2021-04-14 23:34:32 +0930

    Move packages to cloned updated paths

--- Package.swift
+++ Package.swift
@@ -15,9 +15,9 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/kieranb662/Sliders.git" , from: "1.0.3"),
-        .package(url: "https://github.com/kieranb662/Shapes.git", from: "1.0.4"),
-        .package(url: "https://github.com/kieranb662/CGExtender.git", from: "1.0.3")
+        .package(url: "https://github.com/HughEdwards1/Sliders-SwiftUI" , from: "1.0.3"),
+        .package(url: "https://github.com/HughEdwards1/SwiftUI-Shapes", from: "1.0.4"),
+        .package(url: "https://github.com/HughEdwards1/CGExtender", from: "1.0.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
