0f6a25eb601e820babd24730ff54bdabfb9eebe7
Hugh Edwards <hugh@hughedwards.dev>
2021-04-15 00:17:18 +0930

    Try name field in package

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/HughEdwards1/Sliders-SwiftUI" , from: "1.1.2"),
+        .package(name: "Shapes", url: "https://github.com/HughEdwards1/Sliders-SwiftUI" , from: "1.1.2"),
         .package(url: "https://github.com/HughEdwards1/SwiftUI-Shapes", from: "1.1.2"),
         .package(url: "https://github.com/HughEdwards1/CGExtender", from: "1.1.2")
     ],
